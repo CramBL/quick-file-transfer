a28d3453dcd6b4e9d99fb47ddddc56bc87350eb8
Marc Beck König <mbkj@tutamail.com>
2024-06-16 04:13:48 +0200

    refactor and always use buffered TCP stream

--- src/client.rs
+++ src/client.rs
@@ -1,6 +1,6 @@
 use std::{
     fs::{self, File},
-    io::{self, BufReader, BufWriter, Read, StdinLock, Write},
+    io::{self, BufReader, BufWriter, StdinLock, Write},
     net::TcpStream,
     path::Path,
 };
@@ -12,29 +12,26 @@ use crate::{
     TCP_STREAM_BUFSIZE,
 };
 use anyhow::Result;
-use flate2::{
-    bufread::{self, GzEncoder},
-    Compression,
-};
+use flate2::{read::GzEncoder, Compression};
 
 const BUFSIZE_READER: usize = 32 * 1024;
 
 pub fn run_client(cfg: Config) -> Result<()> {
-    let mut tcp_stream = tcp_stream(cfg.address())?;
+    let tcp_stream = tcp_stream(cfg.address())?;
+    let mut buf_tcp_stream = tcp_bufwriter(&tcp_stream);
 
     log::info!("Connection to: {}", cfg.address());
     if let Some(msg) = cfg.message() {
-        let res = tcp_stream.write_all(msg.as_bytes());
+        let res = buf_tcp_stream.write_all(msg.as_bytes());
         log::debug!("Wrote message: {msg}");
         log::debug!("TCP write result: {res:?}");
     }
 
     // On-stack dynamic dispatch
     let (mut stdin_read, mut file_read, mut mmap_read);
-    let bufreader: &mut dyn io::BufRead = match cfg.file() {
+    let bufreader: &mut dyn io::Read = match cfg.file() {
         Some(p) if cfg.use_mmap() => {
-            let ammap_read = MemoryMappedReader::new(p)?;
-            mmap_read = BufReader::new(ammap_read);
+            mmap_read = MemoryMappedReader::new(p)?;
             &mut mmap_read
         }
         Some(p) => {
@@ -49,27 +46,21 @@ pub fn run_client(cfg: Config) -> Result<()> {
 
     let transferred_bytes = match cfg.compression().unwrap_or_default() {
         config::Compression::Lz4 => {
-            lz4_incremental_write_to_tcp_stream::<TCP_STREAM_BUFSIZE>(&tcp_stream, bufreader)?
+            let mut lz4_writer = lz4_flex::frame::FrameEncoder::new(&mut buf_tcp_stream);
+            let total_read =
+                incremental_write_to_stream::<TCP_STREAM_BUFSIZE>(&mut lz4_writer, bufreader)?;
+            lz4_writer.finish()?;
+            total_read
         }
         config::Compression::Gzip => {
-            use flate2::Compression;
             let mut encoder = GzEncoder::new(bufreader, Compression::fast());
-
-            let mut data = [0; TCP_STREAM_BUFSIZE];
-            let mut total_read = 0;
-            loop {
-                let bytes_read = encoder.read(&mut data)?;
-                if bytes_read == 0 {
-                    break;
-                }
-                total_read += bytes_read;
-                tcp_stream.write_all(&mut data[0..bytes_read])?;
-            }
-            total_read as u64
+            incremental_write_to_stream::<TCP_STREAM_BUFSIZE>(&mut buf_tcp_stream, &mut encoder)?
         }
         config::Compression::Bzip2 => todo!(),
         config::Compression::Xz => todo!(),
-        config::Compression::None => copy_all_to_tcp_stream(&tcp_stream, bufreader)?,
+        config::Compression::None => {
+            incremental_write_to_stream::<TCP_STREAM_BUFSIZE>(&mut buf_tcp_stream, bufreader)?
+        }
     };
     log::info!("Wrote {} to stream", format_data_size(transferred_bytes));
 
@@ -93,7 +84,7 @@ pub fn tcp_bufwriter(socket: &TcpStream) -> BufWriter<&TcpStream> {
     BufWriter::with_capacity(BUFSIZE_READER, socket)
 }
 
-pub fn copy_all_to_tcp_stream(socket: &TcpStream, reader: &mut dyn io::BufRead) -> Result<u64> {
+pub fn copy_all_to_tcp_stream(socket: &TcpStream, reader: &mut dyn io::Read) -> Result<u64> {
     let mut writer = tcp_bufwriter(socket);
     let len = io::copy(reader, &mut writer)?;
     Ok(len)
@@ -109,11 +100,18 @@ pub fn lz4_copy_all_to_tcp_stream(socket: &TcpStream, reader: &mut dyn io::BufRe
 
 pub fn lz4_incremental_write_to_tcp_stream<const BUFSIZE: usize>(
     socket: &TcpStream,
-    reader: &mut dyn io::BufRead,
+    reader: &mut dyn io::Read,
 ) -> Result<u64> {
-    use lz4_flex::frame::FrameEncoder;
-    let mut lz4_writer = FrameEncoder::new(socket);
+    let mut lz4_writer = lz4_flex::frame::FrameEncoder::new(socket);
+    let total_read = incremental_write_to_stream::<BUFSIZE>(&mut lz4_writer, reader)?;
+    lz4_writer.finish()?;
+    Ok(total_read as u64)
+}
 
+pub fn incremental_write_to_stream<const BUFSIZE: usize>(
+    stream_writer: &mut dyn io::Write,
+    reader: &mut dyn io::Read,
+) -> Result<u64> {
     let mut buf = [0; BUFSIZE];
     let mut total_read = 0;
     loop {
@@ -123,8 +121,7 @@ pub fn lz4_incremental_write_to_tcp_stream<const BUFSIZE: usize>(
         }
         total_read += bytes_read;
 
-        lz4_writer.write(&buf[..bytes_read])?;
+        stream_writer.write(&buf[..bytes_read])?;
     }
-    lz4_writer.finish()?;
     Ok(total_read as u64)
 }
