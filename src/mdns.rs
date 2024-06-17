06d48ebbc9d6fab0bb0a0c0eb883b6e4b63d3f02
Marc Beck König <mbkj@tutamail.com>
2024-06-17 14:43:36 +0200

    separate out mdns hostname resolving

--- src/mdns.rs
+++ src/mdns.rs
@@ -25,7 +25,7 @@ pub fn handle_mdns_command(cmd: MdnsCommand) -> Result<()> {
         MdnsCommand::Resolve(MdnsResolveArgs {
             hostname,
             timeout_ms,
-        }) => resolve_hostname(&hostname, timeout_ms),
+        }) => resolve_hostname_print_stdout(&hostname, timeout_ms),
         MdnsCommand::Register(MdnsRegisterArgs {
             hostname,
             service_label,
@@ -118,67 +118,15 @@ pub fn discover_service_type(
     Ok(())
 }
 
-pub fn resolve_hostname(hostname: &str, timeout_ms: u64) -> Result<()> {
-    let stopflag = AtomicBool::new(false);
-    let thread_timeout = timeout_ms + 10;
-
-    let mdns = ServiceDaemon::new()?;
+pub fn resolve_hostname_print_stdout(hostname: &str, timeout_ms: u64) -> Result<()> {
     let mut hostname = hostname.to_owned();
     hostname.push_str(".local.");
     log::info!("Resolving address for {hostname}");
-    let receiver = mdns.resolve_hostname(&hostname, Some(timeout_ms))?;
-
-    let mut resolved_info: Option<MdnsServiceInfo> = None;
-    // Receive the events
-    std::thread::scope(|s| {
-        let _resolver_receiver = s.spawn(|| {
-            let mut hostname = None;
-            let mut ip_set = HashSet::<IpAddr>::new();
-            while let Ok(hostname_resolution_event) = receiver.recv() {
-                match hostname_resolution_event {
-                    mdns_sd::HostnameResolutionEvent::SearchStarted(s) => {
-                        log::debug!("Search started: {s}")
-                    }
-                    mdns_sd::HostnameResolutionEvent::AddressesFound(s, recv_ip_set) => {
-                        log::debug!("Hostname found! {s}: {recv_ip_set:?}");
-                        if let Some(h) = hostname.as_deref() {
-                            debug_assert_eq!(h, s);
-                        } else {
-                            hostname = Some(s);
-                        }
-                        ip_set.extend(recv_ip_set);
-                    }
-                    _ => log::debug!("{hostname_resolution_event:?}"),
-                }
-            }
-
-            let info = MdnsServiceInfo::new(
-                hostname.as_deref().unwrap_or_default().to_owned(),
-                None,
-                None,
-                ip_set,
-            );
-            println!("{info}");
-            resolved_info = Some(info);
-            stopflag.store(true, Ordering::Relaxed);
-        });
-        let _resolver_watchdog = s.spawn(|| {
-            // Wait for the timeout duration or until stopflag is set
-            let start_time = std::time::Instant::now();
-            while !stopflag.load(Ordering::Relaxed) {
-                if start_time.elapsed() >= Duration::from_millis(thread_timeout) {
-                    break;
-                }
-                thread::sleep(Duration::from_millis(10));
-            }
-        });
-    });
-    if let Some(resolved_info) = resolved_info {
+    if let Some(resolved_info) = resolve_mdns_hostname(&hostname, timeout_ms)? {
         println!("{resolved_info}");
     } else {
         log::error!("Failed resolving {hostname}");
     }
-    mdns.shutdown()?;
     Ok(())
 }
 
@@ -229,4 +177,57 @@ pub fn start_mdns_service(
     thread::sleep(keepalive_dur);
     mdns.shutdown()?;
     Ok(())
+}
+
+pub fn resolve_mdns_hostname(hostname: &str, timeout_ms: u64) -> Result<Option<MdnsServiceInfo>> {
+    let stopflag = AtomicBool::new(false);
+    let mdns = ServiceDaemon::new()?;
+    let receiver = mdns.resolve_hostname(&hostname, Some(timeout_ms))?;
+
+    let resolved_info = std::thread::scope(|s| {
+        let resolver_receiver = s.spawn(|| {
+            let mut hostname = None;
+            let mut ip_set = HashSet::<IpAddr>::new();
+            while let Ok(hostname_resolution_event) = receiver.recv() {
+                match hostname_resolution_event {
+                    mdns_sd::HostnameResolutionEvent::SearchStarted(s) => {
+                        log::trace!("Search started: {s}")
+                    }
+                    mdns_sd::HostnameResolutionEvent::AddressesFound(s, recv_ip_set) => {
+                        log::debug!("Hostname found! {s}: {recv_ip_set:?}");
+                        if let Some(h) = hostname.as_deref() {
+                            debug_assert_eq!(h, s);
+                        } else {
+                            hostname = Some(s);
+                        }
+                        ip_set.extend(recv_ip_set);
+                    }
+                    _ => log::trace!("{hostname_resolution_event:?}"),
+                }
+            }
+            let info = if let Some(hn) = hostname {
+                Some(MdnsServiceInfo::new(hn, None, None, ip_set))
+            } else {
+                None
+            };
+
+            stopflag.store(true, Ordering::Relaxed);
+            info
+        });
+        let _resolver_watchdog = s.spawn(|| {
+            // Wait for the timeout duration or until stopflag is set
+            let start_time = std::time::Instant::now();
+            while !stopflag.load(Ordering::Relaxed) {
+                if start_time.elapsed() >= Duration::from_millis(timeout_ms) {
+                    break;
+                }
+                thread::sleep(Duration::from_millis(10));
+            }
+        });
+        resolver_receiver
+            .join()
+            .expect("Failed joining resolver receiver thread")
+    });
+
+    Ok(resolved_info)
 }
