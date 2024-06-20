5ab272ebbe0f41cabe367301e864a2a89e40c2bb
Marc Beck König <mbkj@tutamail.com>
2024-06-20 10:35:15 +0200

    refactor mdns discover to use scoped threads

--- src/mdns/discover.rs
+++ src/mdns/discover.rs
@@ -1,23 +1,19 @@
 use anyhow::Result;
 use mdns_sd::{ServiceDaemon, ServiceEvent};
 use std::{
-    sync::{
-        atomic::{AtomicBool, Ordering},
-        Arc,
-    },
+    sync::atomic::{AtomicBool, Ordering},
     thread,
     time::Duration,
 };
 
-use crate::mdns::util::MdnsServiceInfo;
+use crate::mdns::util::{self, MdnsServiceInfo};
 
 pub fn discover_service_type(
     service_label: &str,
     service_protocol: &str,
     timeout_ms: u64,
 ) -> Result<()> {
-    let stopflag = Arc::new(AtomicBool::new(false));
-    let stopflag_child = Arc::clone(&stopflag);
+    let stopflag = AtomicBool::new(false);
 
     let mdns = ServiceDaemon::new()?;
 
@@ -27,58 +23,67 @@ pub fn discover_service_type(
 
     let receiver = mdns.browse(&service_type)?;
 
-    std::thread::spawn(move || {
-        let mut discovered_services: Vec<MdnsServiceInfo> = vec![];
-        loop {
-            if stopflag_child.load(Ordering::Relaxed) {
-                break;
-            }
-            while let Ok(event) = receiver.try_recv() {
-                match event {
-                    ServiceEvent::ServiceResolved(info) => {
-                        log::info!("Resolved a new service: {}", info.get_fullname());
-                        log::debug!("Hostname: {}", info.get_hostname());
-                        log::debug!("IP: {:?}", info.get_addresses());
-                        if let Some(service_info) = discovered_services
-                            .iter_mut()
-                            .find(|s| s.hostname() == info.get_hostname())
-                        {
-                            service_info.add_ips(info.get_addresses());
-                        } else {
-                            discovered_services.push(info.into());
+    let discovered_services: Vec<MdnsServiceInfo> = thread::scope(|s| {
+        let service_info_h = s.spawn(|| {
+            let mut discovered_services: Vec<MdnsServiceInfo> = vec![];
+            loop {
+                if stopflag.load(Ordering::Relaxed) {
+                    break;
+                }
+                while let Ok(event) = receiver.try_recv() {
+                    match event {
+                        ServiceEvent::ServiceResolved(info) => {
+                            log::info!("Resolved a new service: {}", info.get_fullname());
+                            log::debug!("Hostname: {}", info.get_hostname());
+                            log::debug!("IP: {:?}", info.get_addresses());
+                            if let Some(service_info) = discovered_services
+                                .iter_mut()
+                                .find(|s| s.hostname() == info.get_hostname())
+                            {
+                                service_info.add_ips(info.get_addresses());
+                            } else {
+                                discovered_services.push(info.into());
+                            }
                         }
-                    }
-                    other_event => {
-                        log::debug!("Received other event: {:?}", &other_event);
+                        other_event => {
+                            log::debug!("Received other event: {:?}", &other_event);
+                        }
                     }
                 }
             }
-        }
-        log::info!(
-            "Discovered {} service{}!",
-            discovered_services.len(),
-            if discovered_services.len() == 1 {
-                ""
-            } else {
-                "s"
+            log::info!(
+                "Discovered {} service{}!",
+                discovered_services.len(),
+                if discovered_services.len() == 1 {
+                    ""
+                } else {
+                    "s"
+                }
+            );
+            stopflag.store(true, Ordering::Relaxed);
+            discovered_services
+        });
+        s.spawn(|| {
+            // Wait for the timeout duration or until stopflag is set
+            let start_time = std::time::Instant::now();
+            while !stopflag.load(Ordering::Relaxed) {
+                if start_time.elapsed() >= Duration::from_millis(timeout_ms) {
+                    stopflag.store(true, Ordering::Relaxed);
+                    thread::sleep(Duration::from_millis(200));
+                    break;
+                }
+                thread::sleep(Duration::from_millis(10));
             }
-        );
-        for discovered_service in discovered_services.iter() {
-            println!("{discovered_service}");
-        }
-        stopflag_child.store(true, Ordering::Relaxed);
+            util::mdns_daemon_shutdown(&mdns);
+        });
+
+        service_info_h
+            .join()
+            .expect("Failed joining service discovery thread")
     });
 
-    // Wait for the timeout duration or until stopflag is set
-    let start_time = std::time::Instant::now();
-    while !stopflag.load(Ordering::Relaxed) {
-        if start_time.elapsed() >= Duration::from_millis(timeout_ms) {
-            stopflag.store(true, Ordering::Relaxed);
-            thread::sleep(Duration::from_millis(200));
-            break;
-        }
-        thread::sleep(Duration::from_millis(10));
+    for discovered_service in discovered_services.iter() {
+        println!("{discovered_service}");
     }
-    mdns.shutdown()?;
     Ok(())
 }
