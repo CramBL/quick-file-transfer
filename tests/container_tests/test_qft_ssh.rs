7e154014d6a9864dda3d8ce09180af226555e5f7
Marc Beck König <mbkj@tutamail.com>
2024-06-28 09:57:40 +0200

    add ssh test using scp like args formatting

--- tests/container_tests/test_qft_ssh.rs
+++ tests/container_tests/test_qft_ssh.rs
@@ -85,3 +85,44 @@ pub fn test_ssh_transfer_no_tcp_port_specified() -> TestResult {
 
     Ok(())
 }
+
+#[test]
+#[ignore = "Needs to be run with container test (just d-test)"]
+pub fn test_ssh_transfer_no_tcp_port_specified_scp_args() -> TestResult {
+    let dir = TempDir::new()?;
+    let file_to_transfer = dir.child("f-L,sL.txt");
+    const TRANSFERED_CONTENTS: &str = LOREM_IPSUM_WHAT;
+    fs::write(&file_to_transfer, LOREM_IPSUM_WHAT)?;
+    let file_to_receive: String = CONTAINER_HOME_DOWNLOAD_DIR.to_owned() + "/LOREM_WHAT.txt";
+
+    let _test_container = TestContainer::setup("/usr/sbin/sshd -D -p 54320", true);
+
+    let mut cmd = Command::cargo_bin(BIN_NAME).unwrap();
+    let args = [
+        "send",
+        "ssh",
+        &format!("{CONTAINER_USER}@{CONTAINER_IP}:{file_to_receive}"),
+        "--ssh-port",
+        CONTAINER_SSH_PORT,
+        "--file",
+        file_to_transfer.path().to_str().unwrap(),
+        "-vv",
+        "--start-port",
+        CONTAINER_DYNAMIC_PORTS_START,
+        "--end-port",
+        CONTAINER_DYNAMIC_PORTS_END,
+    ];
+    cmd.args(args);
+    let StdoutStderr { stdout, stderr } = process_output_to_stdio(cmd.output()?)?;
+
+    eprint_docker_logs()?;
+    eprint_cmd_args_stderr_stdout_formatted(&args, &stdout, &stderr);
+
+    //ERROR Failed to send to [
+    assert_no_errors_or_warn(&stderr)?;
+
+    let f = assert_file_exists_in_container(&file_to_receive)?;
+    pretty_assert_str_eq!(fs::read_to_string(f)?, TRANSFERED_CONTENTS);
+
+    Ok(())
+}
