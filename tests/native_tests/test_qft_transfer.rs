6284abe51b985fda26af794984857967d0396c06
Marc Beck König <mbkj@tutamail.com>
2024-06-28 09:41:15 +0200

    add tests with output dir and multiple files

--- tests/native_tests/test_qft_transfer.rs
+++ tests/native_tests/test_qft_transfer.rs
@@ -1,3 +1,5 @@
+use fs::read_dir;
+
 use crate::util::*;
 
 pub const IP: &str = "127.0.0.1";
@@ -628,6 +630,159 @@ pub fn test_file_transfer_xz_compr_level_1() -> TestResult {
     assert_no_errors_or_warn(&server_stderr)?;
     assert_no_errors_or_warn(&client_stderr)?;
     pretty_assert_str_eq!(TRANSFERED_CONTENTS, fs::read_to_string(file_to_receive)?);
+
+    Ok(())
+}
+
+#[test]
+pub fn test_file_transfer_output_dir_single_file() -> TestResult {
+    let fname = "f1.txt";
+    // Create a file in temp dir
+    let dir = TempDir::new()?;
+    let file_to_transfer = dir.child(fname);
+    // Create a subdirectory in the temp dir
+    let subdir = dir.join("tmp_subdir");
+    fs::create_dir(&subdir)?;
+    // Get the path and leak it because of lifetimes
+    let subdir_path_as_str = subdir.as_path().to_string_lossy().into_owned();
+    let subdir_path = subdir_path_as_str.leak();
+    // Take the created (empty) subdir and join it with the filename, we expected it to show up here afer the transfer
+    let file_to_receive = subdir.join(fname);
+
+    const TRANSFERED_CONTENTS: &str = "contents";
+    fs::write(&file_to_transfer, TRANSFERED_CONTENTS)?;
+
+    let port = get_free_port(IP).unwrap();
+
+    let server_thread = spawn_server_thread(
+        None,
+        [
+            "--ip",
+            IP,
+            "--port",
+            port.as_str(),
+            "-vv",
+            "--output-dir",
+            subdir_path,
+        ],
+    )?;
+
+    let mut cmd = Command::cargo_bin(BIN_NAME).unwrap();
+    let args = [
+        "send",
+        "ip",
+        IP,
+        "--port",
+        port.as_str(),
+        "-vv",
+        "--file",
+        file_to_transfer.path().to_str().unwrap(),
+    ];
+    cmd.args(args);
+    let StdoutStderr {
+        stdout: _client_stdout,
+        stderr: client_stderr,
+    } = process_output_to_stdio(cmd.output()?)?;
+
+    let StdoutStderr {
+        stdout: _server_stdout,
+        stderr: server_stderr,
+    } = join_thread_and_get_output(server_thread)?;
+
+    assert_no_errors_or_warn(&server_stderr)?;
+    assert_no_errors_or_warn(&client_stderr)?;
+
+    eprintln!("Dir contents");
+    eprintln!("{dir:?}");
+    for e in read_dir(subdir)? {
+        eprintln!("{e:?}");
+    }
+    pretty_assert_str_eq!(TRANSFERED_CONTENTS, fs::read_to_string(file_to_receive)?);
+
+    Ok(())
+}
+
+#[test]
+pub fn test_file_transfer_output_dir_multiple_files() -> TestResult {
+    let f1_name = "f1.txt";
+    let f2_name = "f2.txt";
+    // Create a file in temp dir
+    let dir = TempDir::new()?;
+    let file1_to_transfer = dir.child(f1_name);
+    let file2_to_transfer = dir.child(f2_name);
+    // Create a subdirectory in the temp dir
+    let subdir = dir.join("tmp_subdir");
+    fs::create_dir(&subdir)?;
+    // Get the path and leak it because of lifetimes
+    let subdir_path_as_str = subdir.as_path().to_string_lossy().into_owned();
+    let subdir_path = subdir_path_as_str.leak();
+    // Take the created (empty) subdir and join it with the filename, we expected it to show up here afer the transfer
+    let file1_to_receive = subdir.join(f1_name);
+    let file2_to_receive = subdir.join(f2_name);
+
+    const TRANSFERED_CONTENTS: &str = "contents";
+    fs::write(&file1_to_transfer, TRANSFERED_CONTENTS)?;
+    fs::write(&file2_to_transfer, TRANSFERED_CONTENTS)?;
+    assert!(file1_to_transfer.exists());
+    assert!(file1_to_transfer.exists());
+
+    let port = get_free_port(IP).unwrap();
+
+    let server_thread = spawn_server_thread(
+        None,
+        [
+            "--ip",
+            IP,
+            "--port",
+            port.as_str(),
+            "-vv",
+            "--output-dir",
+            subdir_path,
+        ],
+    )?;
+
+    let mut cmd = Command::cargo_bin(BIN_NAME).unwrap();
+    let args = [
+        "send",
+        "ip",
+        IP,
+        "--port",
+        port.as_str(),
+        "-vv",
+        "--file",
+        file1_to_transfer.path().to_str().unwrap(),
+        "--file",
+        file2_to_transfer.path().to_str().unwrap(),
+    ];
+    cmd.args(args);
+    let StdoutStderr {
+        stdout: _client_stdout,
+        stderr: client_stderr,
+    } = process_output_to_stdio(cmd.output()?)?;
+
+    let StdoutStderr {
+        stdout: _server_stdout,
+        stderr: server_stderr,
+    } = join_thread_and_get_output(server_thread)?;
+
+    eprintln!("=== SERVER ===");
+    eprintln!("=== COMMAND STDOUT ===\n{_server_stdout}\n^^^COMMAND STDOUT^^^\n");
+    eprintln!("=== COMMAND STDERR ===\n{server_stderr}\n^^^COMMAND STDERR^^^\n");
+
+    eprintln!("=== COMMAND ARGUMENTS ===\n{args:?}\n");
+    eprintln!("=== COMMAND STDOUT ===\n{_client_stdout}\n^^^COMMAND STDOUT^^^\n");
+    eprintln!("=== COMMAND STDERR ===\n{client_stderr}\n^^^COMMAND STDERR^^^\n");
+
+    assert_no_errors_or_warn(&server_stderr)?;
+    assert_no_errors_or_warn(&client_stderr)?;
+
+    eprintln!("Dir contents");
+    eprintln!("{dir:?}");
+    for e in read_dir(subdir)? {
+        eprintln!("{e:?}");
+    }
+    pretty_assert_str_eq!(TRANSFERED_CONTENTS, fs::read_to_string(file1_to_receive)?);
+    pretty_assert_str_eq!(TRANSFERED_CONTENTS, fs::read_to_string(file2_to_receive)?);
 
     Ok(())
 }
