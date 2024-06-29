abc67a1a022e9c67d364b789b3a48a58bca7f833
Marc Beck König <mbkj@tutamail.com>
2024-06-29 08:54:01 +0200

    initial new ssh mode

--- src/run.rs
+++ src/run.rs
@@ -1,12 +1,13 @@
 use anyhow::bail;
 
-use crate::config::{Command, Config};
+use crate::config::{ssh::parse_scp_style_uri, Command, Config};
 
 pub fn run(_cfg: &Config) -> anyhow::Result<()> {
     if let Some(ref cmd) = _cfg.command {
         match cmd {
             Command::Listen(ref args) => crate::server::listen(_cfg, args),
             Command::Send(ref cmd) => crate::send::handle_send_cmd(cmd, _cfg),
+            Command::GetFreePort(ref a) => crate::get_free_port::handle_get_free_port(a),
 
             #[cfg(feature = "mdns")]
             Command::Mdns(ref cmd) => crate::mdns::handle_mdns_command(&cmd.subcmd),
@@ -16,7 +17,34 @@ pub fn run(_cfg: &Config) -> anyhow::Result<()> {
                 crate::evaluate_compression::evaluate_compression(args.clone())
             }
 
-            Command::GetFreePort(ref a) => crate::get_free_port::handle_get_free_port(a),
+            #[cfg(feature = "ssh")]
+            Command::Ssh(ref args) => {
+                // Determine if the operation is local to remote or remote to local
+                let is_local_to_remote = args.is_sending();
+                let is_remote_to_local = !args.is_sending();
+
+                let remote_uri_components = if is_local_to_remote {
+                    parse_scp_style_uri(&args.destination)
+                } else {
+                    parse_scp_style_uri(args.sources.first().unwrap())
+                }?;
+                println!("URI: {remote_uri_components:?}");
+
+                println!("Sources: {:?}", args.sources);
+                println!("Destination: {}", args.destination);
+                //println!("Recursive: {}", args.recursive);
+                //println!("Preserve Times: {}", args.preserve_times);
+                //println!("Verbose: {}", args.verbose);
+                println!(
+                    "Operation: {}",
+                    if is_remote_to_local {
+                        "Remote to Local"
+                    } else {
+                        "Local to Remote"
+                    }
+                );
+                Ok(())
+            }
         }
     } else {
         bail!("No subcommand specified")
