55a0bd83d8a98ef786db4d9ab9e2dbf799f33095
Marc Beck König <mbkj@tutamail.com>
2024-07-13 09:56:41 +0200

    add communicating the final server result at end of a transfer (only on main thread)

--- src/server.rs
+++ src/server.rs
@@ -12,9 +12,13 @@ use std::{
 
 use crate::{
     config::{
-        transfer::{command::ServerCommand, listen::ListenArgs},
+        transfer::{
+            command::{ServerCommand, ServerResult},
+            listen::ListenArgs,
+        },
         Config,
     },
+    send::util::send_command,
     util::{
         bind_listen_to_free_port_in_range, create_file_with_len, format_data_size, read_server_cmd,
         server_handshake,
@@ -37,28 +41,15 @@ pub fn listen(_cfg: &Config, listen_args: &ListenArgs) -> Result<()> {
     let stop_flag: Arc<AtomicBool> = Arc::new(AtomicBool::new(false));
     let ip: IpAddr = ip.parse()?;
     let initial_listener = TcpListener::bind((ip, *port))?;
-
-    let mut handles = vec![];
-
-    match run_server(initial_listener, listen_args, &mut handles, &stop_flag) {
-        Ok(()) => join_all_threads(handles),
-        Err(e) => {
-            tracing::warn!("{e}, stopping all threads...");
-            stop_flag.store(true, Ordering::Relaxed);
-            join_all_threads(handles);
-            bail!(e)
-        }
-    }
-
-    Ok(())
+    run_server(initial_listener, listen_args, &stop_flag)
 }
 
 fn run_server(
     initial_listener: TcpListener,
     args: &ListenArgs,
-    thread_handles: &mut Vec<JoinHandle<anyhow::Result<()>>>,
     stop_flag: &Arc<AtomicBool>,
 ) -> anyhow::Result<()> {
+    let mut thread_handles = vec![];
     match initial_listener.accept() {
         Ok((mut socket, addr)) => {
             tracing::info!("Client accepted at: {addr:?}");
@@ -75,6 +66,20 @@ fn run_server(
                             &cmd,
                         )?;
                         thread_handles.push(child_thread_handle);
+                    } else if matches!(cmd, ServerCommand::EndOfTransfer) {
+                        tracing::trace!("Received command: {cmd:?}, stopping all threads...");
+                        stop_flag.store(true, Ordering::Relaxed);
+                        match join_all_threads(thread_handles) {
+                            Ok(_) => {
+                                send_result(&mut socket, &ServerResult::Ok)?;
+                                return Ok(());
+                            }
+                            Err(th_errs) => {
+                                let err_res = ServerResult::err(th_errs.clone());
+                                send_result(&mut socket, &err_res)?;
+                                bail!(th_errs);
+                            }
+                        }
                     }
                 } else {
                     tracing::debug!("Main Client disconnected...");
@@ -87,12 +92,42 @@ fn run_server(
     Ok(())
 }
 
-fn join_all_threads(handles: Vec<JoinHandle<anyhow::Result<()>>>) {
+/// Send a [ServerResult] to the client
+pub fn send_result(stream: &mut TcpStream, result: &ServerResult) -> anyhow::Result<()> {
+    tracing::trace!("Sending result: {result:?}");
+    let result_bytes = bincode::serialize(result)?;
+    debug_assert!(result_bytes.len() <= u8::MAX as usize);
+    let size = result_bytes.len() as u16;
+    let header = size.to_be_bytes();
+
+    // Send the header followed by the command
+    stream.write_all(&header)?;
+    stream.write_all(&result_bytes)?;
+    Ok(())
+}
+
+fn join_all_threads(handles: Vec<JoinHandle<anyhow::Result<()>>>) -> Result<(), String> {
+    let mut errors = String::new();
     for h in handles {
+        let mut h_name = h.thread().name().unwrap_or_default().to_owned();
         match h.join().map_err(|e| format!("{e:?}")) {
             Ok(_) => (),
-            Err(e) => log::error!("Thread joined with error: {e}"),
+            Err(e) => {
+                tracing::error!("Thread {h_name} joined with error: {e}");
+                h_name.push_str(" failed: ");
+                if !errors.is_empty() {
+                    errors.push('\n');
+                }
+                errors.extend(h_name.drain(..));
+                errors.push_str(&e);
+            }
         }
+    }
+    if errors.is_empty() {
+        Ok(())
+    } else {
+        tracing::warn!("{errors}");
+        Err(errors)
     }
 }
 
@@ -121,7 +156,11 @@ fn handle_cmd(cmd: ServerCommand, cfg: &ListenArgs, socket: &mut TcpStream) -> a
             log::debug!("Received file list: {fname:?}");
             util::handle_receive_data(cfg, socket, fname, decompr)?;
         }
+        // TODO: Constrict these to only the main thread.
         ServerCommand::GetFreePort(_) => todo!(),
+        ServerCommand::EndOfTransfer => {
+            unreachable!("Child thread received end of transfer command")
+        }
     }
     Ok(())
 }
@@ -159,11 +198,8 @@ fn run_server_thread(
                 handle_client_socket(cfg, &mut socket)?;
             }
             Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
-                log::trace!("Would block");
-                if cfg!(linux) {
-                    log::trace!("Would block - yielding thread");
-                    std::thread::park_timeout(Duration::from_millis(100));
-                }
+                log::trace!("Would block - yielding thread");
+                std::thread::park_timeout(Duration::from_millis(10));
             }
             Err(e) => {
                 log::error!("{e}");
@@ -203,6 +239,8 @@ fn spawn_server_thread_on_new_port(
         .local_addr()
         .expect("Unable to get local address for TCP listener")
         .port();
+    tracing::trace!("Bound to free port: {free_port}");
+
     let free_port_be_bytes = free_port.to_be_bytes();
     debug_assert_eq!(free_port_be_bytes.len(), 2);
     socket.write_all(&free_port_be_bytes)?;
