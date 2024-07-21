94dba1209cc3e1a221a3d3302c55fe7dee7e2844
Marc Beck König <mbkj@tutamail.com>
2024-07-21 10:16:35 +0200

    add lib.rs to integrate better with criterion benches

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,6 @@
-// Performance lints
-#![warn(variant_size_differences)]
-#![warn(
-    clippy::needless_pass_by_value,
-    clippy::unnecessary_wraps,
-    clippy::mutex_integer,
-    clippy::mem_forget,
-    clippy::maybe_infinite_iter
-)]
-
 use std::process::ExitCode;
 
-pub mod config;
-#[cfg(feature = "evaluate-compression")]
-pub mod evaluate_compression;
-pub mod get_free_port;
-#[cfg(feature = "mdns")]
-pub mod mdns;
-pub mod mmap_reader;
-pub mod send;
-pub mod server;
-#[cfg(feature = "ssh")]
-pub mod ssh;
-pub mod util;
-
-pub mod run;
-pub const TCP_STREAM_BUFSIZE: usize = 2 * 1024;
-pub const BUFFERED_RW_BUFSIZE: usize = 32 * 1024;
-
+use quick_file_transfer::{config, run};
 fn main() -> ExitCode {
     let cfg = match config::Config::init() {
         Ok(cfg) => cfg,
